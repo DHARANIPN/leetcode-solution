d321d27e2b4c41ee45beab5812eff804fce07543
DHARANIPN <pndaglp@gmail.com>
2026-06-01 14:12:29 +0530

    File updated at 6/1/2026, 2:12:30 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,37 +1,27 @@
-// Last updated: 6/1/2026, 1:29:46 PM
+// Last updated: 6/1/2026, 2:12:30 PM
 1class Solution {
 2    public int romanToInt(String s) {
-3        int ans = 0, num = 0;
-4        for (int i = s.length() - 1; i >= 0; i--) {
-5            switch (s.charAt(i)) {
-6                case 'I':
-7                    num = 1;
-8                    break;
-9                case 'V':
-10                    num = 5;
-11                    break;
-12                case 'X':
-13                    num = 10;
-14                    break;
-15                case 'L':
-16                    num = 50;
-17                    break;
-18                case 'C':
-19                    num = 100;
-20                    break;
-21                case 'D':
-22                    num = 500;
-23                    break;
-24                case 'M':
-25                    num = 1000;
-26                    break;
-27            }
-28            if (4 * num < ans)
-29                ans -= num;
-30            else
-31                ans += num;
-32        }
-33        return ans;
-34
-35    }
-36}
+3     int res = 0;
+4     Map<Character, Integer> roman = new HashMap<>();
+5        roman.put('I', 1);
+6        roman.put('V', 5);
+7        roman.put('X', 10);
+8        roman.put('L', 50);
+9        roman.put('C', 100);
+10        roman.put('D', 500);
+11        roman.put('M', 1000);
+12
+13      for (int i = 0; i < s.length() - 1; i++) {
+14            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+15                res -= roman.get(s.charAt(i));
+16            } else {
+17                res += roman.get(s.charAt(i));
+18            }
+19        }
+20
+21        return res + roman.get(s.charAt(s.length() - 1)); 
+22    }
+23}
+24    
+25     
+26

ec9c53e9c2241e25d0b47a56f19b4d6bb9085c77
DHARANIPN <pndaglp@gmail.com>
2026-06-02 21:17:27 +0530

    File updated at 6/2/2026, 9:17:28 PM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,15 +1,25 @@
-// Last updated: 6/2/2026, 8:41:54 PM
+// Last updated: 6/2/2026, 9:17:28 PM
 1class Solution {
-2    public int lengthOfLastWord(String s) {
-3        int i = s.length() - 1;
-4        while (i >= 0 && s.charAt(i) == ' ') {
-5            i--;
-6        }
-7        int end = i;
-8        while (i >= 0 && s.charAt(i) != ' ') {
-9            i--;
-10        }
+2    public int mySqrt(int x) {
+3        if (x < 2) {
+4            return x;
+5        }
+6        int low = 1, high = x / 2, ans = 0;
+7
+8        while (low <= high) {
+9            int mid = low + (high - low) / 2;
+10            long square = (long) mid * mid;
 11
-12        return end - i;
-13    }
-14}
+12            if (square == x) {
+13                return mid;
+14            } else if (square < x) {
+15                ans = mid;
+16                low = mid + 1;
+17            } else {
+18                high = mid - 1;
+19            }
+20        }
+21
+22        return ans;
+23    }
+24}

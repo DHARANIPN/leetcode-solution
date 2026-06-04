dabced0baa98a95afd2a2c402e5913bf3563054d
DHARANIPN <pndaglp@gmail.com>
2026-06-04 11:25:59 +0530

    File updated at 6/4/2026, 11:25:59 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,17 +1,24 @@
-// Last updated: 6/4/2026, 10:48:36 AM
+// Last updated: 6/4/2026, 11:25:59 AM
 1class Solution {
 2    public int maximumUnits(int[][] boxTypes, int truckSize) {
 3        Arrays.sort(boxTypes,(a,b)->b[1]-a[1]);
-4        int ans = 0;
-5        for(int i =0;i<boxTypes.length;i++){
-6            if(boxTypes[i][0]>truckSize){
-7                boxTypes[i][0] = Math.min(boxTypes[i][0],truckSize);
-8            }
-9           ans+=(boxTypes[i][0] * boxTypes[i][1] );
-10           truckSize -= boxTypes[i][0];
-11        }
-12        return ans;
-13
-14    }
-15}
-16
+4        // int ans = 0;
+5        // for(int i =0;i<boxTypes.length;i++){
+6        //     if(boxTypes[i][0]>truckSize){
+7        //         boxTypes[i][0] = Math.min(boxTypes[i][0],truckSize);
+8        //     }
+9        //    ans+=(boxTypes[i][0] * boxTypes[i][1] );
+10        //    truckSize -= boxTypes[i][0];
+11        // }
+12        // return ans;
+13        int ans = 0,i =0,n = boxTypes.length;
+14        while(i<n && truckSize > 0){
+15            int box = Math.min(boxTypes[i][0],truckSize);
+16            ans+=(box * boxTypes[i][1] );
+17            i++;
+18            truckSize -= box;
+19        }
+20        return ans;
+21    }
+22}
+23

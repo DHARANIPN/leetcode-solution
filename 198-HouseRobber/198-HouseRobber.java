e1f5d34efb8c05d37606e80591688d0467ace89e
DHARANIPN <pndaglp@gmail.com>
2026-06-05 15:03:30 +0530

    File updated at 6/5/2026, 3:03:29 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,16 +1,36 @@
-// Last updated: 6/5/2026, 11:33:22 AM
+// Last updated: 6/5/2026, 3:03:29 PM
 1class Solution {
-2    public int deleteAndEarn(int[] nums) {
-3        int[] temp = new int[10001];
-4        for(int i =0;i<nums.length;i++){
-5                temp[nums[i]]+= nums[i];
-6        }
-7        int a =0,b=0,m=0;
-8        for(int r : temp){
-9            m = Math.max(a,b+r);
-10            b=a;
-11            a=m;
-12        }
-13        return m;        
-14        }
-15}
+2    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+3    int [][] dp = new int[startTime.length][3];
+4    for(int i =0;i<startTime.length;i++){
+5        dp[i][0] = startTime[i];
+6        dp[i][1] = endTime[i];
+7        dp[i][2] =profit[i];
+8    }
+9    Arrays.sort(dp,(a,b)->a[0]-b[0]);
+10     Arrays.sort(startTime);
+11     int n = startTime.length;
+12     int[] arr = new int[n+1];
+13     arr[n] = 0;
+14     for(int i = n-1;i>=0;i--){
+15        int cp = dp[i][2];
+16        int end = dp[i][1];
+17        int ind = -1;
+18        int after = 0;
+19        for(int k= i+1;k<n;k++){
+20        if(end<=dp[k][0] ){
+21             ind = k;
+22            break;
+23         }
+24        }
+25         if(ind != -1){
+26            after = arr[ind];
+27         }
+28            int total = cp+after;
+29            arr[i]=Math.max(total,arr[i+1]);
+30         
+31     }   
+32    
+33     return arr[0];
+34   }
+35}

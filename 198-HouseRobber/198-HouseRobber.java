9f497734ff03bf6faa53c3cba8850a2df333fe91
DHARANIPN <pndaglp@gmail.com>
2026-06-05 11:33:22 +0530

    File updated at 6/5/2026, 11:33:22 AM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,12 +1,16 @@
-// Last updated: 6/5/2026, 10:25:05 AM
+// Last updated: 6/5/2026, 11:33:22 AM
 1class Solution {
-2    public int rob(int[] nums) {
-3        int a = 0,b=0,m=0;
-4        for(int i : nums){
-5            m = Math.max(a,b+i);
-6            b=a;
-7            a = m;
-8        }
-9        return m;
-10    }
-11}
+2    public int deleteAndEarn(int[] nums) {
+3        int[] temp = new int[10001];
+4        for(int i =0;i<nums.length;i++){
+5                temp[nums[i]]+= nums[i];
+6        }
+7        int a =0,b=0,m=0;
+8        for(int r : temp){
+9            m = Math.max(a,b+r);
+10            b=a;
+11            a=m;
+12        }
+13        return m;        
+14        }
+15}

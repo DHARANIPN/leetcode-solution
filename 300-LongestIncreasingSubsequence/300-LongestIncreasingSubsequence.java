86bac0546f9765699c81409dcca8d8de84b814f7
DHARANIPN <pndaglp@gmail.com>
2026-06-06 15:41:51 +0530

    File updated at 6/6/2026, 3:41:51 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,24 +1,27 @@
-// Last updated: 6/6/2026, 1:53:11 PM
+// Last updated: 6/6/2026, 3:41:51 PM
 1class Solution {
-2    public int lengthOfLIS(int[] nums) {
-3        //tabulation 
-4        if(nums.length<=1) return nums.length;
-5        int[] arr = new int[nums.length];
-6        arr[nums.length-1] = 1;
-7        int max = 0;
-8    
-9        for(int i =nums.length-2;i>=0;i--){
-10            int ans = 0;
-11            for(int j = i+1;j<nums.length;j++){
-12                
-13                if(nums[i]<nums[j]){
-14                    ans = Math.max(ans,arr[j]);
-15                }
-16            }
-17            arr[i] = ans+1;
-18            max = Math.max(max,arr[i]);
-19        }
-20       return  max;
-21    }
-22    
-23}
+2    public int minDistance(String word1, String word2) {
+3        int[][] arr = new int[word1.length()+1][word2.length()+1];
+4        for(int i = 0;i<word1.length()+1;i++){
+5                arr[i][0] = i;
+6        }
+7        for(int j = 0;j<word2.length()+1;j++){
+8            arr[0][j] = j;
+9        }
+10        for(int i = 1;i<word1.length()+1;i++){
+11            for(int j =1;j<word2.length()+1;j++){
+12             if(word1.charAt(i-1) == word2.charAt(j-1)){
+13                   arr[i][j] = arr[i-1][j-1];
+14             }
+15             else{
+16                  int  m = Math.min(arr[i-1][j-1],arr[i-1][j]);
+17                  arr[i][j] = Math.min(arr[i][j-1],m)+1;
+18             }
+19            }
+20        }
+21        return arr[word1.length()][word2.length()];
+22        
+23    }
+24}
+25
+26

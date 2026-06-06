43f501f5a857f10242d05b71d663169eb23a8de6
DHARANIPN <pndaglp@gmail.com>
2026-06-06 13:53:12 +0530

    File updated at 6/6/2026, 1:53:11 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,18 +1,24 @@
-// Last updated: 6/6/2026, 12:27:35 PM
+// Last updated: 6/6/2026, 1:53:11 PM
 1class Solution {
-2    public int uniquePaths(int m, int n) {
-3        int arr[][] = new int[m][n];
-4        for(int i = 0;i<m;i++) {
-5            for(int j = 0;j<n;j++){
-6                arr[i][j] = 1;
-7            }
-8        }
-9        
-10        for(int i=1;i<m;i++){
-11            for(int j = 1 ;j<n;j++){
-12                    arr[i][j] = arr[i-1][j]+arr[i][j-1];            
-13        }
-14    }
-15      return arr[m-1][n-1];
-16 }
-17}
+2    public int lengthOfLIS(int[] nums) {
+3        //tabulation 
+4        if(nums.length<=1) return nums.length;
+5        int[] arr = new int[nums.length];
+6        arr[nums.length-1] = 1;
+7        int max = 0;
+8    
+9        for(int i =nums.length-2;i>=0;i--){
+10            int ans = 0;
+11            for(int j = i+1;j<nums.length;j++){
+12                
+13                if(nums[i]<nums[j]){
+14                    ans = Math.max(ans,arr[j]);
+15                }
+16            }
+17            arr[i] = ans+1;
+18            max = Math.max(max,arr[i]);
+19        }
+20       return  max;
+21    }
+22    
+23}

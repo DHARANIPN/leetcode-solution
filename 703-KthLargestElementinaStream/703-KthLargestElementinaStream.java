bb2d27c69119356bf73f346d9d6844b9ce36af78
DHARANIPN <pndaglp@gmail.com>
2026-06-28 21:23:07 +0530

    File updated at 6/28/2026, 9:23:06 PM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.java
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.java
@@ -1,30 +1,45 @@
-// Last updated: 6/28/2026, 9:21:35 PM
-1class KthLargest {
-2    private int k;
-3    private PriorityQueue<Integer> minHeap;
-4
-5    public KthLargest(int k, int[] nums) {
-6        this.k = k;
-7        minHeap = new PriorityQueue<>(k);
-8        for (int num : nums) {
-9            if (minHeap.size() < k) {
-10                minHeap.offer(num);
-11            } else if (num > minHeap.peek()) {
-12                minHeap.offer(num);
-13                if (minHeap.size() > k) {
-14                    minHeap.poll();
-15                }
-16            }
-17        }
-18    }
-19
-20    public int add(int val) {
-21        if (minHeap.size() < k) {
-22            minHeap.offer(val);
-23        } else if (val > minHeap.peek()) {
-24            minHeap.offer(val);
-25            minHeap.poll();
-26        }
-27        return minHeap.peek();
-28    }
-29}
+// Last updated: 6/28/2026, 9:23:06 PM
+1class Solution {
+2
+3    int newColor;
+4    int oldColor;
+5
+6    int[][] dir = {
+7        {-1,0},
+8        {1,0},
+9        {0,-1},
+10        {0,1}
+11    };
+12
+13    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+14
+15        newColor = color;
+16        oldColor = image[sr][sc];
+17
+18        if(oldColor != newColor){
+19            dfs(image, sr, sc);
+20        }
+21
+22        return image;
+23    }
+24
+25
+26    public void dfs(int[][] image, int r, int c){
+27
+28        if(r < 0 || c < 0 ||
+29           r >= image.length ||
+30           c >= image[0].length ||
+31           image[r][c] != oldColor){
+32
+33            return;
+34        }
+35
+36
+37        image[r][c] = newColor;
+38
+39
+40        for(int[] d : dir){
+41            dfs(image, r + d[0], c + d[1]);
+42        }
+43    }
+44}

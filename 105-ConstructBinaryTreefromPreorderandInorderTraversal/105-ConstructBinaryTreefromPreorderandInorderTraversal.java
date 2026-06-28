4a62d034684c55d8f1d0752851a8d46a56ad932f
DHARANIPN <pndaglp@gmail.com>
2026-06-28 21:19:50 +0530

    File updated at 6/28/2026, 9:19:50 PM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,24 +1,28 @@
-// Last updated: 6/28/2026, 9:15:21 PM
-class Solution {
-    private int i = 0;
-    private int p = 0;
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, Integer.MIN_VALUE);
-    }
-
-    private TreeNode build(int[] preorder, int[] inorder, int stop) {
-        if (p >= preorder.length) {
-            return null;
-        }
-        if (inorder[i] == stop) {
-            ++i;
-            return null;
-        }
-
-        TreeNode node = new TreeNode(preorder[p++]);
-        node.left = build(preorder, inorder, node.val);
-        node.right = build(preorder, inorder, stop);
-        return node;
-    }
-}
+// Last updated: 6/28/2026, 9:19:50 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode(int x) { val = x; }
+8 * }
+9 */
+10class Solution {
+11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+12         if (root == null || root == p || root == q) {
+13            return root;
+14        }
+15
+16        TreeNode left = lowestCommonAncestor(root.left, p, q);
+17        TreeNode right = lowestCommonAncestor(root.right, p, q);
+18
+19        if (left != null && right != null) {
+20            return root;
+21        }
+22
+23        return left != null ? left : right;        
+24    }
+25}
+26        
+27   

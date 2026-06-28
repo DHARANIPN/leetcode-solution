bd3c5b2df48e4eea2a41e7e8519948fbf6cdb626
DHARANIPN <pndaglp@gmail.com>
2026-06-28 21:27:25 +0530

    File updated at 6/28/2026, 9:27:25 PM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,28 +1,35 @@
-// Last updated: 6/28/2026, 9:19:50 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode(int x) { val = x; }
-8 * }
-9 */
-10class Solution {
-11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-12         if (root == null || root == p || root == q) {
-13            return root;
-14        }
-15
-16        TreeNode left = lowestCommonAncestor(root.left, p, q);
-17        TreeNode right = lowestCommonAncestor(root.right, p, q);
-18
-19        if (left != null && right != null) {
-20            return root;
-21        }
-22
-23        return left != null ? left : right;        
-24    }
-25}
-26        
-27   
+// Last updated: 6/28/2026, 9:27:25 PM
+1class Solution {
+2
+3    String[] map = {
+4        "", "", "abc", "def", "ghi", "jkl",
+5            "mno", "pqrs", "tuv", "wxyz"
+6    };
+7
+8    List<String> res = new ArrayList<>();
+9
+10    public List<String> letterCombinations(String digits) {
+11        if (digits.length() == 0) {
+12            return res;
+13        }
+14
+15        backtrack(digits, 0, new StringBuilder());
+16
+17        return res;
+18    }
+19
+20    private void backtrack(String digits, int idx, StringBuilder sb) {
+21        if (idx == digits.length()) {
+22            res.add(sb.toString());
+23            return;
+24        }
+25
+26        String letters = map[digits.charAt(idx) - '0'];
+27
+28        for (char ch : letters.toCharArray()) {
+29            sb.append(ch);
+30            backtrack(digits, idx + 1, sb);
+31            sb.deleteCharAt(sb.length() - 1);
+32        }
+33    }
+34}

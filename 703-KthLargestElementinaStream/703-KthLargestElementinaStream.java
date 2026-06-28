5a417262cfe3f2fea4d4d9a09396b47cfe7d3e00
DHARANIPN <pndaglp@gmail.com>
2026-06-28 21:29:21 +0530

    File updated at 6/28/2026, 9:29:20 PM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.java
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.java
@@ -1,30 +1,16 @@
-// Last updated: 6/28/2026, 9:27:15 PM
+// Last updated: 6/28/2026, 9:29:20 PM
 1class Solution {
-2    public boolean canFinish(int numCourses, int[][] prerequisites) {
-3        List<List<Integer>> adj = new ArrayList<>();
-4        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
-5
-6        int[] indegree = new int[numCourses];
-7        for (int[] pre : prerequisites) {
-8            adj.get(pre[1]).add(pre[0]);
-9            indegree[pre[0]]++;
-10        }
-11
-12        Queue<Integer> q = new LinkedList<>();
-13        for (int i = 0; i < numCourses; i++)
-14            if (indegree[i] == 0) q.add(i);
-15
-16        int count = 0;
-17        while (!q.isEmpty()) {
-18            int node = q.poll();
-19            count++;
-20
-21            for (int next : adj.get(node)) {
-22                indegree[next]--;
-23                if (indegree[next] == 0) q.add(next);
-24            }
-25        }
-26
-27        return count == numCourses;
-28    }
-29}
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int child=0;
+6        int index=0;
+7        while(index<s.length && child<g.length){
+8            if(s[index]>=g[child]){
+9                child++;
+10            }
+11            index++;
+12        }
+13        return child;
+14    }
+15}

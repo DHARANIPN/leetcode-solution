18c1e9045d55caf61ed70c3d989f6b0bdb599170
DHARANIPN <pndaglp@gmail.com>
2026-07-31 08:27:21 +0530

    File updated at 7/31/2026, 8:27:21 AM

--- 3705-FindGoldenHourCustomers/3705-FindGoldenHourCustomers.java
+++ 3705-FindGoldenHourCustomers/3705-FindGoldenHourCustomers.java
@@ -1,17 +1,19 @@
-// Last updated: 7/31/2026, 8:26:58 AM
+// Last updated: 7/31/2026, 8:27:21 AM
 1class Solution {
-2    public String restoreString(String s, int[] indices) {
-3        
-4        int length=s.length();
-5        StringBuilder sb=new StringBuilder("");
-6         char c[]=new char[length];
-7       
-8        for(int i=0;i<length;i++){
-9
-10            c[indices[i]]=s.charAt(i);
-11
-12        }
-13        sb.append(c);
-14        return sb.toString();
-15    }
-16}
+2    public boolean detectCapitalUse(String word) {
+3        if(word.length() == 0 || word.length() == 1) return true;
+4        
+5        if(Character.isUpperCase(word.charAt(0))){
+6            boolean isFirstCharacter = Character.isUpperCase(word.charAt(1));
+7            for(int i = 2; i < word.length(); i++){
+8                boolean currentCharState = Character.isUpperCase(word.charAt(i));
+9                if(currentCharState != isFirstCharacter) return false;
+10            }
+11        }else{
+12            for(int i = 1; i < word.length(); i++){
+13                if(Character.isUpperCase(word.charAt(i))) return false;
+14            }
+15        }
+16        return true;
+17    }
+18}

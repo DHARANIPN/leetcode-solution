b8e83457ffd9cbfa757b172c217c20bb307e3467
DHARANIPN <pndaglp@gmail.com>
2026-07-31 08:32:10 +0530

    File updated at 7/31/2026, 8:32:10 AM

--- 3705-FindGoldenHourCustomers/3705-FindGoldenHourCustomers.java
+++ 3705-FindGoldenHourCustomers/3705-FindGoldenHourCustomers.java
@@ -1,26 +1,26 @@
-// Last updated: 7/31/2026, 8:30:57 AM
-1class StockSpanner {
-2    Stack<int[]> st = new Stack<>();
+// Last updated: 7/31/2026, 8:32:10 AM
+1/* The isBadVersion API is defined in the parent class VersionControl.
+2      boolean isBadVersion(int version); */
 3
-4    public StockSpanner() {
-5    }
-6
-7    public int next(int price) {
-8        int count = 1;
-9
-10        while (!st.isEmpty() && st.peek()[0] <= price) {
-11            count += st.peek()[1];
-12            st.pop();
-13        }
-14
-15        st.push(new int[]{price, count});
-16
-17        return count;
-18    }
-19}
+4public class Solution extends VersionControl {
+5    public int firstBadVersion(int n) {
+6          int l = 1;
+7        int r = n;
+8
+9        while (l <= r) {
+10            int m = l + (r - l) / 2;
+11
+12            if (isBadVersion(m)) {
+13                // m is bad, but an earlier bad version may exist.
+14                r = m - 1;
+15            } else {
+16                // m is good, so the first bad version is after m.
+17                l = m + 1;
+18            }
+19        }
 20
-21/**
-22 * Your StockSpanner object will be instantiated and called as such:
-23 * StockSpanner obj = new StockSpanner();
-24 * int param_1 = obj.next(price);
-25 */
+21        return l;
+22    }
+23}
+24        
+25

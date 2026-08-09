c1f5979bd84ce61de1baa4a6c3271387b033db98
DHARANIPN <pndaglp@gmail.com>
2026-08-09 09:17:52 +0530

    Create

---
+++ problem-1786247271052/problem-1786247271052.java
@@ -0,0 +1,20 @@
+// Last updated: 8/9/2026, 9:17:51 AM
+1class Solution {
+2    public double minPrice(int[] prices, int[] discounts) {
+3        Arrays.sort(prices);
+4        Arrays.sort(discounts);
+5        double tp = 0.0;
+6        int pi = prices.length-1;
+7        int di = discounts.length-1;
+8        while(pi>=0){
+9            if(di>=0){
+10                tp+=prices[pi]*(100.0 - discounts[di])/100.0;
+11                di--;
+12            }else{
+13                tp += prices[pi];
+14            }
+15            pi--;
+16        }
+17        return tp;
+18    }
+19}
